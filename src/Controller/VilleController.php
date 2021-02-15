02f3bb3897898690906b6b4ef6315ce9c670246e
gaelherambourg <75248829+gaelherambourg@users.noreply.github.com>
2021-02-15 09:30:21 +0100

    Gerer les villes(debut)

--- src/Controller/VilleController.php
+++ src/Controller/VilleController.php
@@ -28,6 +28,25 @@ class VilleController extends AbstractController
         //Créer une instance du form, en lui associant notre entité
         $form_ville = $this->createForm(VilleFormType::class, $ville);
 
+
+        //prends les données du formulaire et les hydrates dans mon entité
+        $form_ville->handleRequest($request);
+
+        //est ce que le formulaire est soumis et valide
+        if($form_ville->isSubmitted() && $form_ville->isValid()){
+
+
+            //déclenche l'update en bdd
+            $entityManager->persist($ville);
+            $entityManager->flush();
+
+            //Créer un message en session
+            $this->addFlash('success', 'La ville a bien été modifié !');
+
+            //Créer une redirection vers une autre page
+            return $this->redirectToRoute('liste_ville');
+        }
+
         return $this->render('ville/listerVille.html.twig', [
             'ville_form'=> $form_ville->createView(),
             'villes' => $villes,
