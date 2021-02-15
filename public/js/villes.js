086020dd0d956bb60874d1fc965f5607aa2558d8
gaelherambourg <75248829+gaelherambourg@users.noreply.github.com>
2021-02-15 16:34:43 +0100

    Gerer les villes(suite)

--- public/js/villes.js
+++ public/js/villes.js
@@ -1,4 +1,70 @@
 $(document).ready(function () {
 
+    $('.supprimer').unbind().click(function (event) {
+        let idVille = $(this).attr("data-ville");
+        let parentToRemove = $(this).parent().parent();
+        console.log(idVille);
+        $.ajax({
+
+            "url": $(".supprVille").attr("data-path"),
+            "data": {
+                "idVille": idVille
+            }
+        })
+            .done(function (Response) {
+                console.log(Response);
+                console.log(Response.status);
+                if (Response.status === "deleted"){
+                    $(parentToRemove).remove();
+                }
+            })
+            .fail(function (Fail){
+                    console.log("titi");
+                    //Impossible de supprimer cette ville
+                    $(".supprVille").prepend("<div class='error'>Impossible de supprimer cette ville, elle est déjà rattachée à des lieux, supprimez les d'abord</div>");
+                })
+            })
+
+    $('.modifier').unbind().click(function (event){
+        let idVille = $(this).attr("data-ville");
+        console.log(idVille);
+
+        $.ajax({
+
+            "url": $(".urlVille").attr("data-path"),
+            "data": {
+                "idVille": idVille
+            }
+        })
+        .done(function (Response) {
+           console.log(Response);
+           let ville = Response
+           $('#ville_form_nom').val(ville.nom);
+           $('#ville_form_codePostal').val(ville.codePostal);
+           $('.listVille').append('<button data-v="'+ville.id+'" id="modifVille">Enregistrer</button>')
+           })
+    })
+
+    $('.supprVille').unbind().on('click', '#modifVille', function (){
+        let idVille = $(this).attr("data-v");
+        let nomVille = $('#ville_form_nom').val();
+        let cpVille = $('#ville_form_codePostal').val();
+        console.log(idVille);
+        $.ajax({
+
+            "url": $(".urlVilleModif").attr("data-path"),
+            "data": {
+                "idVille": idVille,
+                "nomVille": nomVille,
+                "cpVille": cpVille
+            }
+        })
+            .done(function (Response) {
+                console.log(Response);
+                location.reload();
+            })
+
+    })
+
 
 });
--- src/Controller/VilleController.php
+++ src/Controller/VilleController.php
@@ -6,16 +6,19 @@ use App\Entity\Ville;
 use App\Form\VilleFormType;
 use App\Repository\VilleRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use mysql_xdevapi\Exception;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class VilleController extends AbstractController
 {
     /**
-     * @IsGranted("ROLE_USER")
+     * @IsGranted("ROLE_ADMIN")
      * @Route("/admin/ville", name="liste_ville")
      */
     public function listerVille(Request $request,
@@ -38,7 +41,7 @@ class VilleController extends AbstractController
         if($form_ville->isSubmitted() && $form_ville->isValid()){
 
 
-            //déclenche l'update en bdd
+            //déclenche l'insert en bdd
             $entityManager->persist($ville);
             $entityManager->flush();
 
@@ -53,5 +56,96 @@ class VilleController extends AbstractController
             'ville_form'=> $form_ville->createView(),
             'villes' => $villes,
         ]);
+    }
+
+    /**
+     * @IsGranted("ROLE_ADMIN")
+     * @Route("/admin/suppr_ville", name="suppr_ville")
+     */
+    public function SupprimerVille(Request $request,
+                                EntityManagerInterface $entityManager,
+                                VilleRepository $villeRepository): Response
+    {
+
+        $idVille = $request->get('idVille');
+
+        $ville = $entityManager->find(Ville::class, $idVille);
+
+        if (!empty($ville)){
+            try {
+                $entityManager->remove($ville);
+                $entityManager->flush();
+
+                return new JsonResponse([
+                    "status" => "deleted"
+                ], 200);
+            }catch (\Doctrine\DBAL\Exception $exception){
+                $erreur = $exception->getMessage();
+                return new JsonResponse([
+                    "status" => "impossible"
+                ], 400);
+            }
+        }
+    }
+
+    /**
+     * @IsGranted("ROLE_ADMIN")
+     * @Route("/admin/recup_ville", name="recupVille")
+     */
+    public function recupVille(Request $request,
+                               SerializerInterface $serializer,
+                                   EntityManagerInterface $entityManager,
+                                   VilleRepository $villeRepository): Response
+    {
+
+        $idVille = $request->get('idVille');
+
+        $ville = $entityManager->find(Ville::class, $idVille);
+
+        if (!empty($ville)){
+
+            $json = $serializer->serialize($ville, 'json');
+
+            return new JsonResponse($json, 200, [], true);
+        }
+
+    }
+
+    /**
+     * @IsGranted("ROLE_ADMIN")
+     * @Route("/admin/modif_ville", name="modifVille")
+     */
+    public function modifVille(Request $request,
+                               SerializerInterface $serializer,
+                               EntityManagerInterface $entityManager,
+                               VilleRepository $villeRepository): Response
+    {
+
+        $idVille = $request->get('idVille');
+        $nomVille = $request->get('nomVille');
+        $cpVille = $request->get('cpVille');
+
+        $ville = $entityManager->find(Ville::class, $idVille);
+
+        if(!empty($nomVille)){
+            $ville->setNom($nomVille);
+        }
+        if(!empty($cpVille)){
+            $ville->setCodePostal($cpVille);
+        }
+
+        if(!empty($ville->getNom())){
+            if (!empty($ville->getCodePostal())){
+
+                //déclenche l'update en bdd
+                $entityManager->persist($ville);
+                $entityManager->flush();
+
+                $json = $serializer->serialize($ville, 'json');
+
+                return new JsonResponse($json, 200, [], true);;
+
+            }
+        }
     }
 }
