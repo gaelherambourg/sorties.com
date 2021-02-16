a1a65c7297b84211cfd5bfaa6b3a2493e3206f68
Krystor <victor.avron2020@campus-eni.fr>
2021-02-16 22:39:40 +0100

    Modif campus en Ajax, yeah

--- public/js/campus.js
+++ public/js/campus.js
@@ -1,5 +1,66 @@
+
 $(document).ready(function () {
 
+    supprimerCampus();
+
+    modifCampusModal();
+
+    modifierCampus();
+
+function modifCampusModal(){
+    $('#modifCampus').on('show.bs.modal', function (event){
+        let idCampus = $(event.relatedTarget).data('id')
+        let nomCampus = document.getElementById(idCampus).innerText
+        let modal=$(this)
+        modal.find('.modal-body input').val(nomCampus)
+        modal.find('.modal-body p').text(idCampus)
+    })
+}
+
+function modifierCampus(){
+    $('.campusAModif').unbind().click(function (event){
+        let idCampus = $('#idCampus').text()
+        let nomCampus = $('#nomCampus').val()
+
+        $.ajax({
+            url: $('.urlCampusModif').attr('data-path'),
+            data: {
+                'idCampus': idCampus,
+                'nomCampus': nomCampus
+            }
+        })
+            .done(function (Response) {
+                console.log(Response);
+                let ligneAModif= document.getElementById(idCampus)
+                ligneAModif.innerText = nomCampus
+                $('#modifCampus').modal('hide')
+            })
+    })
+}
+/*
+function modifierCampus(){
+    //Récupération de l'Id à modifier.
+    $('.modifCampus').unbind().click(function (event){
+        let idCampus = $(this).attr('data-campus')
+        let nomCampus = $('#form_campus_nom').val();
+
+        $.ajax({
+            url: $('.urlCampusModif').attr('data-path'),
+            data: {
+                'idCampus': idCampus,
+                'nomCampus': nomCampus
+            }
+        })
+            .done(function (Response) {
+                console.log(Response);
+                location.reload();
+            })
+    })
+}
+*/
+
+function supprimerCampus(){
+    //Requête Ajax pour la suppression dynamique d'un campus.
     $('.supprCampus').unbind().click(function (event){
         let idCampus = $(this).attr('data-campus');
         let ligneASupprimer = $(this).parent().parent();
@@ -22,10 +83,12 @@ $(document).ready(function () {
             //Sinon, un message d'erreur est transmis à l'utilisateur.
             .fail(function (Fail){
                 //Impossible de supprimer ce campus.
-                $(".supprCampus").prepend("<div class='error'>Impossible de supprimer ce campus.</div>");
+                $(".supprimerCampus").prepend("<div class='error'>Impossible de supprimer ce campus, lié à un ou plusieurs participants à supprimer en premier lieu.</div>");
             })
 
     })
+}
+
 
 
 
